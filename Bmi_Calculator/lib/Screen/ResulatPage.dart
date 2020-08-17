713646a4383715a1a2792f8328abb87c3acd20dc
islam <islem.bouagada@gmail.com>
2020-08-17 21:38:41 +0200

    Appbar done !

--- Bmi_Calculator/lib/Screen/ResulatPage.dart
+++ Bmi_Calculator/lib/Screen/ResulatPage.dart
@@ -3,8 +3,24 @@ import 'package:flutter/material.dart';
 class ResulatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Color(0xFF1d1e33),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+        elevation: 0,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(20),
+              child: Text(
+                'Your Resultat is :',
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
